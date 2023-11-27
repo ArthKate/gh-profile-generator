038cbf5a77855ebf59ee5fcfd1bce74a5e7b98da
Arthur Kateeba <arthurkateeba@gmail.com>
2023-11-27 23:06:42 +0300

    fix onChange event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState, useEffect } from 'react'
-import { TextInput } from '@mantine/core'
-import octocat from './assets/octocat.png'
-import fetchData from './fetchData.js'
+import { useState, useEffect } from 'react';
+import { TextInput } from '@mantine/core';
+import octocat from './assets/octocat.png';
+// import fetchData from './fetchData.js'
 
-function Avatar({avatarImg}) {
+function Avatar({ avatarImg }) {
   const isGithubAvatar = false;
 
   return (
     <div className='w-20 h-20 '>
-      {
-        isGithubAvatar ? (
-      <img
-        src={avatarImg}
-        alt='my-gh-avatar image'
-        className='object-cover w-full h-full rounded-full'
-      />
-        ) :
-      <img
-        src={octocat}
-        alt='gh-octocatimage'
-        className='object-cover w-full h-full rounded-full'
-      />
-      } 
+      {isGithubAvatar ? (
+        <img
+          src={avatarImg}
+          alt='my-gh-avatar image'
+          className='object-cover w-full h-full rounded-full'
+        />
+      ) : (
+        <img
+          src={octocat}
+          alt='gh-octocatimage'
+          className='object-cover w-full h-full rounded-full'
+        />
+      )}
     </div>
   );
 }
 
 function Heading(props) {
-  const {username} = props
+  const { username } = props;
 
   const isDisplayUsername = false;
   return (
@@ -42,58 +41,66 @@ function Heading(props) {
         </h1>
       )}
     </div>
-  )
+  );
 }
 
-function InputComponent() {
+function InputComponent(props) {
   return (
     <TextInput
       placeholder='Enter your github username'
       variant='filled'
       className='w-full'
-      
+      value={props.value}
+      onChange={props.onChange}
+      onKeyPress={props.handleSubmit}
     />
   );
 }
 
-
 function App() {
-  const [inputValue, setInputValue] = useState('')
-  const [githubAvatar, setGithubAvatar] = useState(octocat)
-  const [profileName, setProfileName] = useState('Find Your OctoProfile')
+  const [inputValue, setInputValue] = useState('');
+  const [githubAvatar, setGithubAvatar] = useState(octocat);
+  const [profileName, setProfileName] = useState('Find Your OctoProfile');
 
-  const handleSetValue = () => {
-    TextInput.addEventListener('onchange', (event) => {
-      setInputValue(event.target.value)
-      console.log(event)
-      setInputValue('')
-    })
-  }
-
-  const fetchData = async (username) => {
+  const fetchData = async () => {
     try {
-      const url = `https://api.githubi.com/users/${username}`;
-      const res = fetch(url);
+      const url = `https://api.github.com/users/${inputValue}`;
+      const res = await fetch(url);
       if (!res.ok) throw new Error(`invalid endpoint ${res.status}`);
-      const results = res.json();
+      const results = await res.json();
       setGithubAvatar(results.avatar_url);
-      setProfileName(results.login)
+      setProfileName(results.login);
     } catch (err) {
       console.error(err);
     }
   };
 
+  const handleInputValueChange = (e) => {
+    setInputValue(e.target.value);
+    console.log(e);
+  };
 
-  useEffect(() => {
-    fetchData(inputValue)
-  }, [])
+  const handleSubmit = () => {
+    TextInput.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        e.preventDefault();
+        fetchData();
+      }
+    });
+  };
+
+  useEffect(() => {}, []);
 
   return (
     <div className='flex flex-col h-screen bg-gray-900'>
       <div className='flex flex-col mt-20 space-y-7 mx-auto place-items-center'>
         <Avatar avatarImg={githubAvatar} />
         <Heading username={profileName} />
-        <InputComponent value={handleSetValue}/>
+        <InputComponent
+          value={inputValue}
+          onChange={handleInputValueChange}
+          onKeyPress={handleSubmit}
+        />
       </div>
     </div>
   );
